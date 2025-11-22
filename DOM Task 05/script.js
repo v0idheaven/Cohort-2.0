61ab6d7a72a10fe3f2585742bcdcbc670b0ae8f7
v0idheaven <dahiyavarun2007@gmail.com>
2025-11-22 17:31:17 +0530

    insta like feature

---
+++ DOM Task 05/script.js
@@ -0,0 +1,19 @@
+var img = document.querySelector('img')
+var love = document.querySelector('#love')
+
+img.addEventListener('dblclick', function () {
+
+    love.style.opacity = 1
+    love.style.transform = 'translate(-50%,-50%) scale(1) rotate(0deg)'
+
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-300%) scale(1) rotate(60deg)'
+    }, 800)
+    setTimeout(function () {
+        love.style.opacity = 0
+    }, 1000)
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+    }, 1200)
+
+})
